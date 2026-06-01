9c646c3ed6079c2118fcf8b0b43e11745e647192
Max Abbot <max.abbot@p42.co.nz>
2026-06-01 23:52:10 +1200

    Update default.nix

--- hosts/work-laptop/default.nix
+++ hosts/work-laptop/default.nix
@@ -32,10 +32,10 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_AC = lib.mkForce "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_SCALING_GOVERNOR_ON_AC = lib.mkDefault "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = lib.mkDefault "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = lib.mkDefault "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = lib.mkDefault "power";
       WIFI_PWR_ON_BAT = 5;
       NMI_WATCHDOG = 0;
     };
